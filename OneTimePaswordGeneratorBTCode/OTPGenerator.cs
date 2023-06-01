270e377910cdc0bf2f4823161ca73354c0f158f4
dragossutac <dragos.sutac@gmail.com>
2023-06-01 14:51:33 +0300

    Add project files.

---
+++ OneTimePaswordGeneratorBTCode/OTPGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Runtime.Intrinsics.Arm;
+
+namespace OneTimePasswordGeneratorBTCode
+{
+    internal class OTPGenerator
+    {
+        private const int ValidityPeriodInSeconds = 30;
+
+        public OneTimePassword GeneratePassword(string userId, DateTime dateTime)
+        {
+            string data = userId + dateTime.ToString("yyyyMMddHHmmss");
+
+            var currentTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+            var expirationTimestamp = currentTimestamp + ValidityPeriodInSeconds;
+
+            string expirationTimestampString = expirationTimestamp.ToString();
+
+            string password = HashSHA256(data + expirationTimestampString);
+
+            return new OneTimePassword(password, expirationTimestamp);
+        }
+        public bool VerifyPasswordExpiration(OneTimePassword otp)
+        {
+            var currentTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+            return currentTimestamp <= otp.ExpirationTimestamp;
+        }
+
+        private string HashSHA256(string input)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(input);
+            byte[] hash = SHA256.HashData(bytes);
+            return BitConverter.ToString(hash).Replace("-", string.Empty);
+        }
+    }
+    public class OneTimePassword
+    {
+        public string Password { get; }
+        public long ExpirationTimestamp { get; }
+
+        public OneTimePassword(string password, long expirationTimestamp)
+        {
+            Password = password;
+            ExpirationTimestamp = expirationTimestamp;
+        }
+    }
+}
---
+++ OneTimePaswordGeneratorBTCode/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OneTimePasswordGeneratorBTCode;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string userId = "dragos.sutac";
+        DateTime dateTime = DateTime.Now;
+
+        OTPGenerator generator = new OTPGenerator();
+        OneTimePassword otp = generator.GeneratePassword(userId, dateTime);
+
+        Console.WriteLine("Generated Password: " + otp.Password);
+
+        bool isValid = generator.VerifyPasswordExpiration(otp);
+        Console.WriteLine("Password Validity: " + isValid);
+
+        System.Threading.Thread.Sleep(5000);
+
+        isValid = generator.VerifyPasswordExpiration(otp);
+        Console.WriteLine("Password Validity after 5 seconds: " + isValid);
+
+        System.Threading.Thread.Sleep(10000);
+
+        isValid = generator.VerifyPasswordExpiration(otp);
+        Console.WriteLine("Password Validity after 15 seconds: " + isValid);
+
+        System.Threading.Thread.Sleep(16000);
+
+        isValid = generator.VerifyPasswordExpiration(otp);
+        Console.WriteLine("Password Validity after 31 seconds: " + isValid);
+
+        Console.ReadKey();
+    }
+}
